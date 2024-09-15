41be8fb680ba165e50f79942ae98ae87a38cca37
having11 <gatoninja236@gmail.com>
2024-09-15 00:23:20 -0500

    add core module def

--- tsDefinitions/Module.ts
+++ tsDefinitions/Module.ts
@@ -2,17 +2,21 @@ import type { ModuleConfigOption } from './ModuleConfigOption';
 import type { ModuleConnectionType } from './ModuleConnectionType';
 import type { ModulePayloadConfig } from './ModulePayloadConfig';
 
-export type Module = {
+export type ModuleCore = {
   displayName: string;
   type: string;
   category: string;
-  connectionTypes: ModuleConnectionType[];
-  storeLink?: string;
   heroImgUrl: string;
+  connectionTypes: ModuleConnectionType[];
+};
+
+export type Module = ModuleCore & {
+  storeLink?: string;
   additionalImgUrls?: string[];
   wiringDiagramImgUrl?: string;
   headerSource?: string;
   datasheet?: string;
+  description?: string;
   configOptions?: ModuleConfigOption[];
   tags: string[];
   payloadConfig: ModulePayloadConfig;
